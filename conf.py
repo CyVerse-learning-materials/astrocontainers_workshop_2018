5297db808e1fd41949bbf13dd7e333b62f372d7b
Chi-kwan Chan <chanc@email.arizona.edu>
2018-04-25 00:51:02 -0400

    Initial commit

---
+++ conf.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# General information about the project.
+
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
+
+from misc.cyverse_sphinx_conf import *  # noqa
+
+project = 'CyVerse AstroContainers Workshop: Learning Docker and Singularity'
+copyright = '2018, CyVerse'
+author = 'CyVerse'
+version = '0.1.0'
+release = '0.1.0'
+
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
